15f83328d69161846daeb917ad83d4234f220914
IvanGavlik <ivangavlik963@gmail.com>
2021-10-08 23:16:44 +0200

    init BE

---
+++ predict/src/main/kotlin/com/hellena/predict/HellenaApplication.kt
@@ -0,0 +1,11 @@
+package com.hellena.predict
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HellenaApplication
+
+fun main(args: Array<String>) {
+	runApplication<HellenaApplication>(*args)
+}
---
+++ predict/src/test/kotlin/com/hellena/predict/HellenaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hellena.predict
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HellenaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
