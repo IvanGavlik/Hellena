2ec662cccd3ea1c0d2fd39fc1b00167d77c9547a
IvanGavlik <ivangavlik963@gmail.com>
2021-11-29 23:04:14 +0100

    navigation

--- predict/src/main/angular/hellena/src/app/app.module.ts
+++ predict/src/main/angular/hellena/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NavigationModule} from './navigation/navigation.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NavigationModule,
     BrowserModule,
     AppRoutingModule
   ],
