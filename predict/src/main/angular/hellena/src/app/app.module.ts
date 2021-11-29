e0d9d5ae9e379c6609f7fb6f85ba5836a40ec9fc
IvanGavlik <ivangavlik963@gmail.com>
2021-11-29 23:08:16 +0100

    header

--- predict/src/main/angular/hellena/src/app/app.module.ts
+++ predict/src/main/angular/hellena/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavigationModule} from './navigation/navigation.module';
+import {HeaderComponent} from "./header/header/header.component";
+import {HeaderModule} from "./header/header.module";
 
 @NgModule({
   declarations: [
@@ -11,6 +13,7 @@ import {NavigationModule} from './navigation/navigation.module';
   ],
   imports: [
     NavigationModule,
+    HeaderModule,
     BrowserModule,
     AppRoutingModule
   ],
