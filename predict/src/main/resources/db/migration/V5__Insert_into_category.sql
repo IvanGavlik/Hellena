3cdff41eb4614d6e695c4239bbb6b74f40e573bd
IvanGavlik <ivangavlik963@gmail.com>
2021-12-13 22:48:12 +0100

    insert into category

---
+++ predict/src/main/resources/db/migration/V5__Insert_into_category.sql
@@ -0,0 +1,76 @@
+delete from category;
+
+insert into category
+values ('1', 'Dječji svijet', 'Dječji svijet', null);
+
+insert into category
+values ('2', 'Pazim što jedem', 'Pazim što jedemt', null);
+
+insert into category
+values ('3', 'Voće i povrće', 'Voće i povrće', null);
+
+insert into category
+values ('4', 'Mliječni proizvodi i jaja', 'Mliječni proizvodi i jaja', null);
+
+insert into category
+values ('5', 'Meso i riba', 'Meso i riba', null);
+
+insert into category
+values ('6', 'Delikatesa', 'Delikatesa', null);
+
+insert into category
+values ('7', 'Pića', 'Pića', null);
+
+insert into category
+values ('8', 'Pekarnica', 'Pekarnica', null);
+
+insert into category
+values ('9', 'Slatkiši i grickalice', 'Slatkiši i grickalice', null);
+
+insert into category
+values ('10', 'Smrznuta hrana', 'Smrznuta hrana', null);
+
+insert into category
+values ('11', 'Priprema jela', 'Priprema jela', null);
+
+insert into category
+values ('12', 'Tjestenina, riža, njoki, tortilje', 'Tjestenina, riža, njoki, tortilje', null);
+
+insert into category
+values ('13', 'Umaci i začini', 'Umaci i začini', null);
+
+insert into category
+values ('14', 'Brzi obrok', 'Brzi obrok', null);
+
+insert into category
+values ('15', 'Konzervirano i juhe', 'Konzervirano i juhe', null);
+
+insert into category
+values ('16', 'Priprema kolača', 'Priprema kolača', null);
+
+insert into category
+values ('17', 'Pahuljice, namazi, kave, čajevi', 'Pahuljice, namazi, kave, čajevi', null);
+
+insert into category
+values ('18', 'Kućni ljubimci', 'Kućni ljubimci', null);
+
+insert into category
+values ('19', 'Čišćenje i pospremanje', 'Čišćenje i pospremanje', null);
+
+insert into category
+values ('20', 'Njega i higijena', 'Njega i higijena', null);
+
+insert into category
+values ('21', 'Kućanske potrepštine', 'Kućanske potrepštine', null);
+
+insert into category
+values ('22', 'Posuđe', 'Posuđe', null);
+
+insert into category
+values ('25', 'Party', 'Party', null);
+
+insert into category
+values ('23', 'Škola i ured', 'Škola i ured', null);
+
+insert into category
+values ('24', 'Auto program', 'Auto program', null);
