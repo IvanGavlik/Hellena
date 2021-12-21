c7d2fbe6889473b90f0db7c9b8e2967324c2f5e5
IvanGavlik <ivangavlik963@gmail.com>
2021-12-21 20:17:21 +0100

    serialize params start

--- predict/src/main/kotlin/com/hellena/predict/api/ItemApiServiceImpl.kt
+++ predict/src/main/kotlin/com/hellena/predict/api/ItemApiServiceImpl.kt
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class ItemApiServiceImpl(val itemService: ItemService): ItemApiDelegate {
@@ -18,7 +19,14 @@ class ItemApiServiceImpl(val itemService: ItemService): ItemApiDelegate {
         return ResponseEntity(itemService.getItems(), headers, HttpStatus.OK)
     }
 
-    override fun getCategories(): ResponseEntity<List<CategoryDto>> {
+     fun getCategories(): ResponseEntity<List<CategoryDto>> {
+        val headers = HttpHeaders()
+        headers.accessControlAllowOrigin = "*"
+        return ResponseEntity(itemService.getCategories(), headers, HttpStatus.OK)
+    }
+
+    override fun getCategories(test: kotlin.Any): ResponseEntity<List<CategoryDto>> {
+        println("decode " + String(Base64.getDecoder().decode(test.toString())));
         val headers = HttpHeaders()
         headers.accessControlAllowOrigin = "*"
         return ResponseEntity(itemService.getCategories(), headers, HttpStatus.OK)
