1f85bb99f8f16e1a6171d78347d99c5d4b16770a
IvanGavlik <ivangavlik963@gmail.com>
2021-12-22 22:22:36 +0100

    card container ui

--- predict/src/main/angular/hellena/src/app/ui/ui.module.ts
+++ predict/src/main/angular/hellena/src/app/ui/ui.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { CardContainerComponent } from './card-container/card-container.component';
 
 
 
 @NgModule({
   declarations: [
-    CardComponent
+    CardComponent,
+    CardContainerComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    CardComponent
+    CardComponent,
+    CardContainerComponent
   ]
 })
 export class UiModule { }
