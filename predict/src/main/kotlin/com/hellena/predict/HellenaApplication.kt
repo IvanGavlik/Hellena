b2b005b673ae935cc35ecc426e09d1954e41c98c
IvanGavlik <ivangavlik963@gmail.com>
2022-01-05 18:33:33 +0100

    cors

--- predict/src/main/kotlin/com/hellena/predict/HellenaApplication.kt
+++ predict/src/main/kotlin/com/hellena/predict/HellenaApplication.kt
@@ -1,12 +1,7 @@
 package com.hellena.predict
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication(scanBasePackages = ["com.hellena.predict.*"])
 class HellenaApplication
@@ -16,23 +11,4 @@ fun main(args: Array<String>) {
 }
 
 
-@Configuration
-class WebServerConfiguration {
 
-	@Value("\${cors.originPatterns:default}")
-	private val corsOriginPatterns: String = "*"
-
-	@Bean
-	fun addCorsConfig(): WebMvcConfigurer {
-		return object : WebMvcConfigurer {
-			override fun addCorsMappings(registry: CorsRegistry) {
-				val allowedOrigins = corsOriginPatterns.split(",").toTypedArray()
-				registry.addMapping("/**")
-					.allowedMethods("*")
-					.allowedOriginPatterns(*allowedOrigins)
-					.allowCredentials(true)
-			}
-		}
-	}
-
-}
