c7ee16cb951d2ff51176981eaabfab4ad949b98f
IvanGavlik <ivangavlik963@gmail.com>
2022-01-28 01:33:54 +0100

    change to CriteriaBuilder

--- predict/src/main/kotlin/com/hellena/predict/item/Item.kt
+++ predict/src/main/kotlin/com/hellena/predict/item/Item.kt
@@ -1,15 +1,20 @@
 package com.hellena.predict.item
 
+import com.hellena.predict.item.QItem.Companion.item
 import com.hellena.predict.item.category.Category
 import com.hellena.predict.item.price.Price
 import com.hellena.predict.item.store.Store
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.PagingAndSortingRepository
+import java.math.BigDecimal
+import java.util.function.Predicate
+import java.util.stream.Collectors.toList
 import javax.persistence.*
 import javax.persistence.criteria.CriteriaBuilder
 import javax.persistence.criteria.CriteriaQuery
 import javax.persistence.criteria.Root
+import kotlin.math.min
 
 @Entity
 @Table(name = "item")
@@ -28,11 +33,11 @@ data class Item(
     @Column(name = "description", nullable = false)
     val description: String,
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(nullable = false)
     val category: Category,
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
         name = "item_store",
         joinColumns = [JoinColumn(name = "item_id", )],
@@ -40,7 +45,7 @@ data class Item(
     )
     val store: List<Store>,
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(nullable = false)
     val price: Price,
     ) {
@@ -60,47 +65,84 @@ class SearchItemRepositoryImpl(
         val query: CriteriaQuery<Item> = builder.createQuery(Item::class.java)
         val root: Root<Item> = query.from(Item::class.java)
 
-        query.select(root)
-            .where(
-                builder.equal(root.get<String>("name"), "test"),
-            )
+        query.select(root);
 
-        return entityManager.createQuery(query).resultList;
+        // TODO sql for this (query)
+        var skip = 0L;
+        if (search.page.getIndex() > 0) {
+            skip = search.page.getIndex() * search.page.getSize()
+        }
+
+
+        return entityManager.createQuery(query).resultList.stream()
+            .filter { filterByName(it, search.name) }
+            .filter { filterByCity(it, search.cityName) }
+            .filter { filterByCategory(it, search.categoryIds) }
+            .filter { filterByStore(it, search.storeIds) }
+            .filter { filterByPriceMIn(it, search.priceMIn) }
+            .filter { filterByPriceMax(it, search.priceMIn) }
+            .skip(skip)
+            .limit(search.page.getSize())
+            .collect(toList());
     }
 
-    /**
-
-    val queryBuilder = BooleanBuilder();
-
-    var today: LocalDate = LocalDate.now();
-    queryBuilder.and(item.price.activeFrom.loe(today))
-    queryBuilder.and(item.price.activeTo.goe(today));
-
-    return queryBuilder;
-
-
-     */
-    private fun setUpPredicate(builder: CriteriaBuilder,root: Root<Item>, search: ItemSearch) {
-        if (search.name != null) {
-            builder.and( builder.equal(root.get<String>("name"), search.name) )
+    private fun filterByName(item: Item, name: String?) : Boolean {
+        var byName = true;
+        if (name != null) {
+            byName = name?.contains(name);
         }
-        if (search.categoryIds.isNotEmpty()) {
-            root.get<Category>("category").`in`(search.categoryIds);
-        }
-        if (search.cityName != null && search.cityName.isEmpty()) {
-            //            queryBuilder.and( item.store..id.`in` (search.cityIds))
-        }
-        if(search.storeIds.isNotEmpty()) {
-            //            queryBuilder.and( item.store contains(search.storeIds) )
-        }
-        if (search.priceMIn != null && search.priceMIn.toDouble() > 0) {
-            queryBuilder.and( item.price.actionPrice.goe(search.priceMIn))
-        }
-        if (search.priceMax != null && search.priceMax.toDouble() > 0) {
-            queryBuilder.and( item.price.actionPrice.loe(search.priceMax))
-        }
+        return byName;
+    }
 
+    private fun filterByCity(item: Item, city: String?) : Boolean {
+        var byCity = true;
+        if(city != null) {
+            byCity = item.store.stream()
+                .filter( { it.location.city.equals(city)  } )
+                .findFirst()
+                .isPresent
+        }
+        return byCity;
+    }
 
+    private fun filterByCategory(item: Item, categoryIds: List<Long>) : Boolean {
+        var byCat = true;
+        if(categoryIds.isNotEmpty()) {
+            byCat = categoryIds.stream()
+                .filter({ it == item.category.id })
+                .findFirst()
+                .isPresent;
+        }
+        return byCat;
+    }
+
+    private fun filterByStore(item: Item, storeIds: List<Long>) : Boolean {
+        var byStore = true;
+        if(storeIds.isNotEmpty()) {
+            byStore = false;
+            item.store.forEach {
+                if (storeIds.contains(it.id)) {
+                    byStore = true;
+                }
+            }
+        }
+        return byStore;
+    }
+
+    private fun filterByPriceMIn(item: Item, minPrice: BigDecimal?) : Boolean {
+        var price = true;
+        if (minPrice != null) {
+            price = item.price.actionPrice.equals(minPrice);
+        }
+        return price;
+    }
+
+    private fun filterByPriceMax(item: Item, maxPrice: BigDecimal?) : Boolean {
+        var price = true;
+        if (maxPrice != null) {
+            price = item.price.actionPrice.equals(maxPrice);
+        }
+        return price;
     }
 }
 
